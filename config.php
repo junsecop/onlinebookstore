dfece375e6caee0dbc40797b9d8147fa74a77256
ALBERT THOMAS <heythismeo@duck.com>
2025-04-21 06:55:17 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$conn = mysqli_connect('sql206.infinityfree.com','if0_38427064','Xzy7fr2hq9kw3P','if0_38427064_bookstore') or die('connection failed');
+?>
+
